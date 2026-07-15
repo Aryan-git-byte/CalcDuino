6d33580bafa2c56d435b80d3b372f6775a9542f7
aryan-git-byte <aryan17550@gmail.com>
2026-07-15 23:18:34 +0530

    updated the coke for wokwi ir remote

--- calcduino.ino
+++ calcduino.ino
@@ -1,7 +1,10 @@
+// include libs
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.h>
 #include <Keypad.h>
 //#include <Adafruit_LiquidCrystal.h>
+
+// defining ir pin
 #define IR_RECEIVER_PIN 11
 
 //defining lcd
@@ -11,11 +14,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 //defining keypad dimension
 const byte rows = 4;
 const byte columns = 4;
+
+// defining variables
 long currentnumber;
 long firstnumber;
 long lastnumber;
 int digit;
 char operation;
+bool numberEntered = false;
 
 //defining buttons
 char keys[rows][columns] = {
@@ -28,7 +34,7 @@ char keys[rows][columns] = {
 //defining buttons to digital pins
 byte rowPins[rows] = {2,3,4,5};
 byte colPins[columns] = {6,7,8,9};
-
+// declaring the keypad
 Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, columns);
 
 void setup()
@@ -50,7 +56,7 @@ void setup()
   lcd.print("Calculator init");
   delay(1500);
   lcd.clear();
-  
+  pinMode(13, OUTPUT);
   IrReceiver.begin(IR_RECEIVER_PIN);
 }
 
@@ -68,34 +74,35 @@ void loop()
 {
   char input = '\0';
 
-
+// ir remote input
   if(IrReceiver.decode()){
     input = decodeRemote(IrReceiver.decodedIRData.command);
     IrReceiver.resume();
   }
-
+// keypad input
   char customKey = myKeypad.getKey();
   if (customKey){
     input = customKey;
   }
-
+// serial printing for debugging
   if (input !='\0') {
     // lcd.print(input);
     Serial.println(input);
   }
-
+// first number input
   if (operation == '\0' && input >= '0' && input <= '9') {
     digit = input - '0';
     if (currentnumber <= 9999999) {
     currentnumber = currentnumber * 10 + digit;
     }
+    numberEntered = true;
     lcd.setCursor(0, 0);
     lcd.print("       ");
     lcd.setCursor(0, 0);
-    lcd.print(currentnumber);  
+    lcd.print(currentnumber); 
   }
-
-  if (input == '-' || input == '+' || input == '*' || input == '/') {
+// operator input
+  if ((input == '-' || input == '+' || input == '*' || input == '/' ) && numberEntered) {
     if (operation == '\0') {
     firstnumber = currentnumber;
     currentnumber = 0;
@@ -105,7 +112,7 @@ void loop()
     lcd.setCursor(8, 0);
     lcd.print(input);
   }
-
+// second number input
   if (operation != '\0'&& input >= '0' && input <= '9') {
     digit = input - '0';
     if (currentnumber <= 9999999) {
@@ -116,7 +123,7 @@ void loop()
     lcd.setCursor(9, 0);
     lcd.print(currentnumber); 
   }
-
+// performing the operation
   if (input == '=') {
     lastnumber = currentnumber;
     switch (operation) {
@@ -129,6 +136,7 @@ void loop()
       case '*':
       currentnumber = firstnumber*lastnumber;
       break;
+      // division case with error handling for division by zero
       case '/':
         if (lastnumber != 0) {
           currentnumber = firstnumber/lastnumber;
@@ -143,17 +151,18 @@ void loop()
      reset_lcd();
      lcd.print(currentnumber);
 
-
+// ressetting all the variables for next operation
      firstnumber = 0;
      lastnumber = 0; 
      currentnumber = 0;
      operation = '\0';
+// beep
+     digitalWrite(13, HIGH);
+     delay(500);
+     digitalWrite(13, LOW);
     }
     
-
-    
-  
-
+// clear
   if (input == 'c') {
     currentnumber = 0;
     reset_lcd();  
@@ -165,42 +174,42 @@ void reset_lcd(){
   lcd.clear();
   lcd.setCursor(0,0);
 }
-
+// function to decode the ir remote input
 char decodeRemote(uint8_t ir_input){
   switch (ir_input) {
   case 48:
   return '1';
-  case 17:
-  return '2';
-  case 18:
-  return '3';
-  case 20:
-  return '4';
-  case 21:
-  return '5';
-  case 22:
-  return '6';
   case 24:
+  return '2';
+  case 122:
+  return '3';
+  case 16:
+  return '4';
+  case 56:
+  return '5';
+  case 90:
+  return '6';
+  case 66:
   return '7';
-  case 25:
+  case 74:
   return '8';
-  case 26:
+  case 82:
   return '9';
-  case 12:
+  case 104:
   return '0';
-  case 0:
+  case 162:
   return 'c';
+  case 226:
+  return '=';
   case 2:
-  return '=';
-  case 1:
   return '+';
-  case 4:
+  case 224:
   return '*';
-  case 5:
+  case 168:
   return '=';
-  case 6:
+  case 144:
   return '/';
-  case 9:
+  case 152:
   return '-';
   default:
   return '\0';
